745161d8d31f6e2e1bbf2a865df81ea57ffe718c
Bhavanvir Rai <bhavanvir.r@gmail.com>
2022-11-28 22:56:14 -0800

    🚧 Began fixing styling for the drop-down button.

--- src/Components/Header/DropdownExampleSearchDropdown.tsx
+++ src/Components/Header/DropdownExampleSearchDropdown.tsx
@@ -1,32 +1,26 @@
 import { useEffect, useRef, useState } from "react";
-import "./DropdownExampleSearchDropdown.css";
 import { HeaderCss, LoginLink, Logo } from "./style";
 import Button from 'react-bootstrap/Button';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
-//let menuRef = useRef();
-
-// function DropdownItem(props: any) {
-//   return (
-//     <li className='dropdownItem'>
-//       <Logo to="/" aria-label="Dogs- Home">
-//         <a> {props.text} </a>
-//       </Logo>
-//     </li>
-//   );
-// }
+import { ButtonGroup } from "react-bootstrap";
+import styled, { keyframes } from "styled-components";
 
 export const DropdownExampleSearchDropdown = (props: any) => {
   const [selectedThread, setThread] = useState("Support Dogs Thread")
   const [open, setOpen] = useState(false);
 
   return (
-    // <div>
-    <DropdownButton id="dropdown-basic-button" title="Selected Thread">
-      <Dropdown.Item href="/">Support Dogs Thread</Dropdown.Item>
-      <Dropdown.Item href="#/action-2">Textbooks Thread</Dropdown.Item>
-      <Dropdown.Item href="#/action-3">Housing Thread</Dropdown.Item>
-    </DropdownButton>
-    // </div>
+    <Dropdown as={ButtonGroup}>
+      <Button variant="dark">Thread</Button>
+
+      <Dropdown.Toggle split variant="dark" id="dropdown-split-basic" />
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="/">Support Dogs</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Textbooks</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Housing</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
   )
 }
