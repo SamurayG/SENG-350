c8cb62c79991fa3adb17cfc97deeaddbe26584e9
Omar Attia <omarmoh272001@gmail.com>
2022-11-28 18:46:36 -0800

    fix: fixing droplist

--- src/Components/Header/DropdownExampleSearchDropdown.tsx
+++ src/Components/Header/DropdownExampleSearchDropdown.tsx
@@ -2,42 +2,31 @@ import { useEffect, useRef, useState } from "react";
 import "./DropdownExampleSearchDropdown.css";
 import { HeaderCss, LoginLink, Logo } from "./style";
 import Button from 'react-bootstrap/Button';
-
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 //let menuRef = useRef();
 
-function DropdownItem(props: any) {
-  return (
-    <li className='dropdownItem'>
-      <Logo to="/" aria-label="Dogs- Home">
-        <a> {props.text} </a>
-      </Logo>
-    </li>
-  );
-}
+// function DropdownItem(props: any) {
+//   return (
+//     <li className='dropdownItem'>
+//       <Logo to="/" aria-label="Dogs- Home">
+//         <a> {props.text} </a>
+//       </Logo>
+//     </li>
+//   );
+// }
 
 export const DropdownExampleSearchDropdown = (props: any) => {
   const [selectedThread, setThread] = useState("Support Dogs Thread")
   const [open, setOpen] = useState(false);
 
   return (
-    <div className='menu-container' >
-      <div className='menu-trigger' onClick={() => { setOpen(!open) }}>
-        <Logo to="/" aria-label="Dogs- Home">
-        <div className="mb-2">
-          <Button className="button button5" variant="outline-primary"><p>{selectedThread} {'▼'}</p> </Button>
-        </div>
-        </Logo>
-      </div>
-
-      <div className={`dropdown-menu ${open ? 'active' : 'inactive'}`} >
-        <h3>Threads you are subscribed to<br /></h3>
-        <ul>
-          <DropdownItem text={"Support Dogs Thread"} />
-          <DropdownItem text={"Textbooks Thread"} />
-          <DropdownItem text={"Housing Thread"} />
-          <DropdownItem text={"Academic Advising Thread"} />
-        </ul>
-      </div>
-    </div>
+    // <div>
+    <DropdownButton id="dropdown-basic-button" title="Selected Thread">
+      <Dropdown.Item href="/">Support Dogs Thread</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">Textbooks Thread</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">Housing Thread</Dropdown.Item>
+    </DropdownButton>
+    // </div>
   )
 }
